6b2b1097eae708fd60dd59795084fc992b89351b
hmtmcse <hmtm.cse@gmail.com>
2023-10-04 22:30:10 +0600

    working on pweb ui and auth

--- pweb_ui/controller/operator_controller.py
+++ pweb_ui/controller/operator_controller.py
@@ -19,6 +19,26 @@ def create():
     return operator_ssr_service.create("operator/form", create_action_url=create_action_url, failed_redirect_url=failed_redirect_url)
 
 
+@operator_controller.route("/update/<int:id>", methods=['POST', 'GET'])
+def update(id: int):
+    update_action_url = url_for("operator_controller.update", id=id)
+    failed_redirect_url = url_for("operator_controller.list")
+    return operator_ssr_service.update("operator/form", update_action_url=update_action_url, failed_redirect_url=failed_redirect_url, model_id=id)
+
+
+@operator_controller.route("/delete/<int:id>", methods=['GET'])
+def delete(id: int):
+    redirect_url = url_for("operator_controller.list")
+    return operator_ssr_service.delete(model_id=id, redirect_url=redirect_url)
+
+
+@operator_controller.route("/details/<int:id>", methods=['GET'])
+def details(id: int):
+    redirect_url = url_for("operator_controller.list")
+    return operator_ssr_service.details(view_name="operator/details", model_id=id, redirect_url=redirect_url)
+
+
 @operator_controller.route("/list", methods=['GET'])
 def list():
-    return ssr_ui_render(view_name="operator/list")
+    search_fields: list = ["name", "email", "username"]
+    return operator_ssr_service.list(view_name="operator/list", search_fields=search_fields)

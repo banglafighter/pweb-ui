25fdbd969269c6363669163e40359f6c782850c4
Touhid Mia <touhid@banglafighter.com>
2023-12-19 22:24:16 +0600

    Load from session

--- pweb_ui/common/pweb_ui_sys.py
+++ pweb_ui/common/pweb_ui_sys.py
@@ -1,27 +1,29 @@
+from pweb_auth.security.pweb_session import PWebSession
 from pweb_ssr import PWebJinjaUtil
 
 
 class PWebUISys:
     PWEB_INSTANCE = None
-    LEFT_NAVIGATION: list = []
+    PWEB_NAV_NAME = "PWEB_NAV_NAME"
     ENABLE_DEFAULT_LEFT_NAV: bool = True
 
     @staticmethod
     def add_to_nav(name: str, url: str, icon_class: str = ""):
-        PWebUISys.LEFT_NAVIGATION.append({
-            "name": name, "url": url, "icon": icon_class
-        })
+        left_navigation: list = PWebSession.get(PWebUISys.PWEB_NAV_NAME, [])
+        left_navigation.append({"name": name, "url": url, "icon": icon_class})
         if PWebUISys.PWEB_INSTANCE:
-            PWebUISys.register_left_nav(PWebUISys.PWEB_INSTANCE, PWebUISys.LEFT_NAVIGATION)
+            PWebUISys.register_left_nav(PWebUISys.PWEB_INSTANCE, left_navigation)
+        PWebSession.add(PWebUISys.PWEB_NAV_NAME, left_navigation)
 
     @staticmethod
     def clear_left_nav():
-        PWebUISys.LEFT_NAVIGATION = []
+        left_navigation = []
+        PWebSession.add(PWebUISys.PWEB_NAV_NAME, left_navigation)
         if PWebUISys.PWEB_INSTANCE:
-            PWebUISys.register_left_nav(PWebUISys.PWEB_INSTANCE, PWebUISys.LEFT_NAVIGATION)
+            PWebUISys.register_left_nav(PWebUISys.PWEB_INSTANCE, left_navigation)
 
     @staticmethod
     def register_left_nav(pweb_app, navigation_list: list = None):
         if not navigation_list:
-            navigation_list = PWebUISys.LEFT_NAVIGATION
+            navigation_list: list = PWebSession.get(PWebUISys.PWEB_NAV_NAME, [])
         PWebJinjaUtil.register_global_variable(pweb_app, {"navigation": navigation_list}, is_update=True)
